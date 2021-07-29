f1939eda1b5e46ffacda5fe0a27c2bf5d4750d29
Karan11000 <76610086+Karan11000@users.noreply.github.com>
2021-07-29 09:54:21 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,9 @@
+function explore(){
+    let show = document.getElementById('intca')
+        if(show.style.display!='none'){
+            show.style.display = 'none';
+        }else{
+            show.style.display = 'flex';
+        }
+    }       
+
